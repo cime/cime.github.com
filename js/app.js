7d333afc0ad697dddc755b592c81546de61f9afc
cime <cime@specialec.net>
2012-12-14 19:42:59 +0100

    Projects page

--- js/app.js
+++ js/app.js
@@ -3,4 +3,14 @@ $(function(){
 		e.preventDefault();
 		print();
 	});
+	
+	$('ul.nav li').click(function(e){
+		e.preventDefault();
+		
+		$('ul.nav li').removeClass('active');
+		$('div.container > div.page').hide();
+		
+		$($('a', this).attr('href')).show();
+		$(this).addClass('active');
+	});
 });
