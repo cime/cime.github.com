f4fac44f892c9135ea7aebc633042c8de16a0bbc
cime <cime@specialec.net>
2013-11-23 00:55:42 +0100

    GitHub repositories under Projects

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+Repository = Backbone.Model.extend({
+
+});
+
+Repositories = Backbone.Collection.extend({
+	model: Repository,
+	url: 'https://api.github.com/users/cime/repos',
+	
+	initialize: function(){
+		
+	}
+});
+
 $(function(){
 	$('#printBtn').click(function(e){
 		e.preventDefault();
@@ -7,10 +20,29 @@ $(function(){
 	$('ul#main-nav li').click(function(e){
 		e.preventDefault();
 		
+		var $this = $(this);
+		
 		$('ul#main-nav li').removeClass('active');
 		$('div.container > div.page').hide();
 		
-		$($('a', this).attr('href')).show();
-		$(this).addClass('active');
+		$($('a', $this).attr('href')).show();
+		$this.addClass('active');
+	});
+	
+	var github = new Repositories();
+
+	github.fetch().done(function(e){
+		_.each(e, function(m){
+			console.dir(m);
+			if(!!!m.fork){
+				var project = $('<li class="list-group-item">' +
+					'<h4 class="list-group-item-heading">' + m.name + ' <small>(<a href="' + m.html_url + '">' + m.html_url + '</a>)</small></h4>' +
+					'<p class="list-group-item-text">' + m.description + '</p>' +
+					'<p class="list-group-item-text"><small>Language: <strong>' + m.language + '</strong></small></p>' +
+					'<small><i class="fa fa-git"></i>' + m.git_url + '</small>' +
+				'</li>')
+				$('#github').append(project);
+			}
+		});
 	});
 });
