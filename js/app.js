bd431399d50533f28f669d16683bc702a0461371
Andrej Cimperšek <cime@specialec.net>
2014-07-08 21:22:47 +0200

    Backbone replaced by Angular

--- js/app.js
+++ js/app.js
@@ -1,3 +1,101 @@
+var app = angular.module('app', ['ngRoute', 'ngAnimate', 'ngResource'])
+
+app.config(function ($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode(false).hashPrefix('!');
+  
+  $routeProvider.when('/about', {
+		templateUrl: 'about.html',
+		controller: 'AboutCtrl'
+    }).when('/blog', {
+		templateUrl: 'blog.html',
+		controller: 'BlogCtrl'
+    }).when('/projects', {
+		templateUrl: 'projects.html',
+		controller: 'ProjectsCtrl'
+    }).otherwise({
+      redirectTo: '/about'
+    });
+});
+
+app.directive('activeLink', ['$location', function($location) {
+    return {
+        restrict: 'A',
+        link: function(scope, elem, attrs) {
+            var path = attrs.activeLink ? 'activeLink' : 'href';
+            var target = angular.isDefined(attrs.activeLinkParent) ? elem.parent() : elem;
+            var disabled = angular.isDefined(attrs.activeLinkDisabled) ? true : false;
+            var nested = angular.isDefined(attrs.activeLinkNested) ? true : false;
+
+            function inPath(needle, haystack) {
+                var current = (haystack == needle);
+                if (nested) {
+                    current |= (haystack.indexOf(needle + '/') == 0);
+                }
+
+                return current;
+            }
+
+            function toggleClass(linkPath, locationPath) {
+                // remove hash prefix and trailing slashes
+                linkPath = linkPath ? linkPath.replace(/^#!/, '').replace(/\/+$/, '') : '';
+                locationPath = locationPath.replace(/\/+$/, '');
+
+                if (linkPath && inPath(linkPath, locationPath)) {
+                    target.addClass('active');
+                    if (disabled) {
+                        target.removeClass('disabled');
+                    }
+                } else {
+                    target.removeClass('active');
+                    if (disabled) {
+                        target.addClass('disabled');
+                    }
+                }
+            }
+
+            // watch if attribute value changes / evaluated
+            attrs.$observe(path, function(linkPath) {
+                toggleClass(linkPath, $location.path());
+            });
+
+            // watch if location changes
+            scope.$watch(
+                function() {
+                    return $location.path(); 
+                }, 
+                function(newPath) {
+                    toggleClass(attrs[path], newPath);
+                }
+            );
+        }
+    };
+}
+]);
+
+app.controller('MainCtrl', function($scope, $location){
+	$scope.isCurrentPath = function (path) {
+      return $location.path() == path;
+    };
+});
+
+app.controller('AboutCtrl', function($scope, $resource){
+	$scope.pageClass = 'page-home';
+});
+
+app.controller('BlogCtrl', function($scope, $resource){
+	$scope.pageClass = 'page-about';
+});
+
+app.controller('ProjectsCtrl', function($scope, $resource){
+	$scope.pageClass = 'page-contact';
+	
+	var repositories = $resource('https://api.github.com/users/cime/repos');
+	repositories.query().$promise.then(function(repos){
+		$scope.repositories = repos;
+	});
+});
+
+/*
 App = {
 	View: {},
 	Model: {},
@@ -103,3 +201,4 @@ $(function(){
 		App.router.navigate($(this).attr('href'), {trigger: true});
 	});
 });
+*/
