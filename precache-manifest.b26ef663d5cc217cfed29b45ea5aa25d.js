dcf32447dd49a3e9573c2e65ff3e5e3eea19b645
cime <cime3d@gmail.com>
2020-01-17 22:03:59 +0100

    Updates

---
+++ precache-manifest.b26ef663d5cc217cfed29b45ea5aa25d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401adc91f5a31c40715c206bf7f98acb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b1a848b9ba428454c31",
+    "url": "/static/css/main.1d4a6ac9.chunk.css"
+  },
+  {
+    "revision": "fa9120290de62db18e40",
+    "url": "/static/js/2.4fa6583e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.4fa6583e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b1a848b9ba428454c31",
+    "url": "/static/js/main.2073e6b5.chunk.js"
+  },
+  {
+    "revision": "f2d2adda5a348c87697a",
+    "url": "/static/js/runtime-main.e26c9861.js"
+  },
+  {
+    "revision": "165eb22735231ef4afb9a4d273cddfc0",
+    "url": "/static/media/avatar.165eb227.png"
+  }
+]);
